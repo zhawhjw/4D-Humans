261a0954a7377489ab0a7d9c7b7ba1226f49e9c1
FantasticApple <zhawhjw@users.noreply.github.com>
2025-02-17 06:29:06 -0500

    update:remove egl in renderer.py

--- hmr2/utils/renderer.py
+++ hmr2/utils/renderer.py
@@ -1,6 +1,6 @@
 import os
-if 'PYOPENGL_PLATFORM' not in os.environ:
-    os.environ['PYOPENGL_PLATFORM'] = 'egl'
+# if 'PYOPENGL_PLATFORM' not in os.environ:
+#     os.environ['PYOPENGL_PLATFORM'] = 'egl'
 import torch
 import numpy as np
 import pyrender
